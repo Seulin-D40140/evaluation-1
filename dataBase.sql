199313d46b5232a52676308ffd80bf034f832189
Donovan Seulin <nel640@hotmail.fr>
2024-02-07 13:08:16 +0100

    creation table users et customer

--- dataBase.sql
+++ dataBase.sql
@@ -23,3 +23,28 @@ INSERT INTO T_Stages ( Name , Description , Duration , Type , UnitaryPrice ) VAL
 																					( "Javascript" , "js avancer" , 28 , "presential" , 230 ),
 																					( "Html" , "html simple" , 9 , "remote" , 90 ),
 																					( "Css" , "stylesheet" , 5 , "remote" , 50 );
+																					
+CREATE TABLE T_Users 
+(
+	idUser			int(4)			PRIMARY KEY AUTO_INCREMENT,
+	Login			varchar(30)		NOT NULL,
+	Password		varchar(50)		NOT NULL
+) ENGINE = InnoDB;
+
+INSERT INTO T_Users ( Login , Password ) VALUES ( "toto40" , "taratoto40" );
+
+CREATE TABLE T_Customers
+(
+	idCustomer			int(4)		PRIMARY KEY AUTO_INCREMENT,
+	Name			varchar(20)		NOT NULL,
+	LastName		varchar(20)		NOT NULL,
+	Email 			varchar(50)		NOT NULL,
+	Phone			int(10)			NOT NULL,
+	Adress			varchar(50)		NOT NULL
+) ENGINE = InnoDB;
+
+INSERT INTO T_Customers ( Name , LastName , Email , Phone , Adress ) VALUES ( "toto" , "tara" , "TaraT@hotmail.fr" , 0568542582 , "12 rue des roses 40260 dax");
+
+ALTER TABLE t_Users ADD COLUMN idCustomer INT(4);
+ALTER TABLE t_Users ADD FOREIGN KEY(idCustomer) REFERENCES T_Customers(idCustomer);
+update t_Users set idCustomer=1 where idUser=1;
